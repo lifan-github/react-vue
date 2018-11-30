28112c8ebb519fb43aed03801663f8c0cde38284
李帆 <18266913301@163.com>
2018-11-30 17:47:02 +0800

    订单数据优化

--- my-vue/src/store/modules/order.js
+++ my-vue/src/store/modules/order.js
@@ -1,22 +1,32 @@
 
 import API from '../../api/api';
+import config from "../../utils/config";
+import ElementUI from "element-ui";
 
 const Order = {
   state: {
-    orders: []
+    orders: {}, //包含页面
   },
   mutations: {
     //更改 Vuex 的 store 中的状态的唯一方法是提交 mutation
     updateOrders(state, res){
-      console.log(res,'res----');
+      console.log(res,'stroe--->>需要更新的数据');
       state.orders = res;
     }
   },
   actions: {
-    getAllOrdersApi(context){
-      API.get()
+    getAllOrdersApi(context, data){
+      const params = data.params;
+      API.get(config.url + 'orders', {
+        params: params,
+      })
         .then(res => {
-          context.commit('updateOrders', res.data.data)
+          console.log(res,'res');
+          if(res.data.code === 200){
+            context.commit('updateOrders', res.data)
+          }else{
+            ElementUI.Message.error(res.data.message)
+          }
         })
         .catch(err => {
           console.log(err,'err')
--- my-vue/src/views/orders/Orders.vue
+++ my-vue/src/views/orders/Orders.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    <div>orders</div>
-    <ol>
-      <li v-for="item in orderList">
-        {{item.title}}
-      </li>
-    </ol>
+    <el-table
+            ref="multipleTable"
+            :data="tableData"
+            tooltip-effect="dark"
+            style="width: 100%"
+            @selection-change="handleSelectionChange">
+      <el-table-column
+              type="selection"
+              width="55">
+      </el-table-column>
+      <el-table-column
+              prop="date_placed"
+              label="日期"
+              width="160">
+      </el-table-column>
+      <el-table-column
+              prop="product_shipping_address.name"
+              label="姓名"
+              width="120">
+      </el-table-column>
+      <el-table-column
+              prop="product_shipping_address.address"
+              label="地址"
+              show-overflow-tooltip>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -14,21 +34,34 @@
     name: "Orders",
     data(){
       return {
-
+        multipleSelection: []
       }
     },
     computed: {
-      orderList(){
-        return this.$store.state.orderModule.orders
+      tableData(){
+        return this.$store.state.orderModule.orders.data
       }
     },
     created(){
-      console.log("组件加载did");
       //触发一个事件
-      // this.$store.dispatch('getAllOrdersApi');
+      let params = {
+        page: 1,
+        limit: 10,
+        order_number: '',
+        sort_by: 'order_date',
+        keyword: '',
+        order_status: '',
+        shipping_status: '',
+        paymethod: '',
+        channel: '',
+        month: ''
+      };
+      this.$store.dispatch('getAllOrdersApi', {params: params});
     },
     methods: {
-
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      }
     }
   }
 </script>
